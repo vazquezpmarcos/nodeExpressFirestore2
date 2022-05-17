319f8c580d74813ad681fa479e1462f48439db75
vazquezpmarcos <vazquezpmarcos@gmail.com>
2022-05-17 13:01:03 +0200

    post

---
+++ index.js
@@ -0,0 +1,35 @@
+const admin = require("firebase-admin");
+const cred = require("./cert.json")
+admin.initializeApp({credential: admin.credential.cert(cred)});
+const db = admin.firestore();
+
+const express = require("express");
+const { QuerySnapshot } = require("@google-cloud/firestore");
+const app = express();
+app.use(express.json());
+
+const port = 3000;
+
+app.post("/usuarios", (req, res) => {
+    const users = req.body;
+    db.collection("users").add(users)
+    res.send("Usuario añadido")
+});
+
+app.get("/usuarios", (req, res) => {
+    db.collection("users")
+    .get()
+    .then(
+        (QuerySnapshot) => {
+            const users = [];
+            QuerySnapshot.forEach((doc) => {
+                users.push(doc.data());
+            });
+            res.send(users);
+        });
+    }
+    );
+
+app.listen(port, () => {
+    console.log("puerto funcionando")
+});
