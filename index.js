3eb9d0bf4039eca8fe5eaa12fb59cde792b5ddb0
vazquezpmarcos <vazquezpmarcos@gmail.com>
2022-05-18 13:10:33 +0200

    put + delete

--- index.js
+++ index.js
@@ -8,19 +8,42 @@ const { QuerySnapshot } = require("@google-cloud/firestore");
 const app = express();
 app.use(express.json());
 
-const port = 3000;
+const port = 4000;
 
 app.post("/usuarios", (req, res) => {
     const users = req.body;
-    db.collection("users").doc("Marcos").set(users)
-    res.send("Usuario añadido")
+    db.collection("users").add(users)
+    res.send("Usuario")
 });
 
 app.put("/usuarios", (req, res) => {
-    const users = req.body;
-    db.collection("users").doc("Marcos").set(users)
-    res.send("Usuario modificado")
-});
+    db.collection("users").where("email", "==", "vazquezpmarcos@gmail.com2")
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            res.send(doc.id);
+            const newEmail = req.body;
+            db.collection("users").doc(doc.id).update(newEmail)
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+})
+
+app.delete("/usuarios", (req, res) => {
+    db.collection("users").where("email", "==", "vazquezpmarcos@gmail.com")
+    .get()
+    .then(function(querySnapshot) {
+        querySnapshot.forEach(function(doc) {
+            res.send(doc.id);
+            db.collection("users").doc(doc.id).delete()
+        });
+    })
+    .catch(function(error) {
+        console.log("Error getting documents: ", error);
+    });
+})
 
 app.get("/usuarios", (req, res) => {
     db.collection("users")
@@ -35,6 +58,8 @@ app.get("/usuarios", (req, res) => {
         });
     }
     );
+
+    
 
 app.listen(port, () => {
     console.log("puerto funcionando")
